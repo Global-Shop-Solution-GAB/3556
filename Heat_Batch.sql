247cc264d2b0329106af7e992adf214a8322a3a8
Service Web <sw@gssmail.com>
2024-03-08 13:07:15 -0600

    Commit changes in C:\Websites\Git\emarlow\3556\Heat_Batch.sql

---
+++ Heat_Batch.sql
@@ -0,0 +1,13 @@
+-- Database: GLOBALASW Metadata version: 2 Exported: Aug 16, 2019
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "HEAT_BATCH" USING 'HEAT_BATCH.mkd' PAGESIZE=4096 (
+ "SERIAL_NUMBER" CHAR(30),
+ "REC_ID" CHAR(10),
+ "BATCH" INTEGER,
+ "EMPL_LAST_CHANGE" CHAR(20),
+ "DATE_LAST_CHANGE" DATE );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
